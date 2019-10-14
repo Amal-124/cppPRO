9359a0879c94c918133cf894bfd02cfc62e4fc44
Amal-124 <55346261+Amal-124@users.noreply.github.com>
2019-10-14 19:28:42 +0400

    Update Con+Des2.cpp

--- Con+Des2.cpp
+++ Con+Des2.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
 using namespace std;
-class Line 
+class student 
 {  
-public:
+ public:
+    //setters
 	void setid(int ID);
 	void setname(string NAME);
 	void setgpa(double GPA);
+	//getters
 	int getid();
 	double getgpa();
 	string getname();
-	
 	student(int id); //constructer
-private:
+ private:
 	int id;
 	string name;
-	double gpa;
+	double gpa; };
+	
+student::student(int ID){ id =ID;}
+// initiliazing the getters
+int student::getid(void){ return id;}
+string student::getname(void){ return name;}
+double student::getgpa(void){ return gpa;}
+// initiliazing the setters
+void student::setid(int ID){ id= ID;}
+void student::getname(string name){ name =NAME;}
+void student::getgpa(double gpa){ gpa= GPA;}
 
-};
-student::student(int ID)
-{
-id =ID;	
-}
-double Line::getlength(void){ return length;}
 int main(){
-	Line l;
-	l.setlength(6.0);
-	cout<<"length of line : "<<l.getlength() << endl;	
+	student s;
+	s.setgpa(6.0);
+	cout<<"length of line : "<<s.getgpa() << endl;	
 	return 0;
 }
